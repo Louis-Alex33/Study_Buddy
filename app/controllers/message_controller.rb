8323583305d23f17583028f776a37a4d417d355e
Louis Alexandre Richoux <louisalex.richoux@gmail.com>
2025-12-02 12:03:43 +0100

    add LLM

---
+++ app/controllers/message_controller.rb
@@ -0,0 +1,29 @@
+class MessageController < ApplicationController
+
+    def create
+    if @message.save
+    @ruby_llm_chat = RubyLLM.chat
+    build_conversation_history
+    response = @ruby_llm_chat.with_instructions(instructions).ask(@message.content)
+
+    @chat.messages.create(role: "assistant", content: response.content)
+    @chat.generate_title_from_first_message
+    else
+    render "chats/show", status: :unprocessable_entity
+    end
+  end
+
+  def process_file(file)
+    if file.content_type == "application/pdf"
+    @ruby_llm_chat = RubyLLM.chat(model: "gemini-2.0-flash")
+    build_conversation_history
+    @ruby_llm_chat.with_instructions(instructions)
+    @response = @ruby_llm_chat.ask(@message.content, with: { pdf: @message.file.url })
+    elsif file.image?
+    @ruby_llm_chat = RubyLLM.chat(model: "gpt-4o") # vision-capable model
+    build_conversation_history
+    @ruby_llm_chat.with_instructions(instructions)
+    @response = @ruby_llm_chat.ask(@message.content, with: { image: @message.file.url })
+    end
+  end
+end
