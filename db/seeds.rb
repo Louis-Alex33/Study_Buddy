a05b63987528592aa65f565b53ce4d90d6a60167
Leopold Gosse <leopoldgosse@icloud.com>
2025-12-03 14:57:35 +0100

    bob10

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,34 +12,33 @@ henry = User.create!(
 
 category_1 = Category.create!(
   title: "Droit",
-  user: henry,
   created_at: "2025-11-01",
 )
 category_2 = Category.create!(
   title: "Anatomie",
-  user: henry,
   created_at: "2025-11-01",
 )
 category_3 = Category.create!(
   title: "Informatique",
-  user: henry,
   created_at: "2024-03-11",
 )
 
 category_4 = Category.create!(
   title: "Robotique",
-  user: henry,
   created_at: "2024-03-12",
 )
 
 category_5 = Category.create!(
   title: "Environnement",
-  user: henry,
   created_at: "2025-06-12",
 )
 
 category_6 = Category.create!(
   title: "Génie civil",
-  user: henry,
   created_at: "2023-02-29",
+)
+lecture = Lecture.create!(
+  title: "Test",
+  category: category_1,
+  resume: "Ceci est un cours test"
 )
