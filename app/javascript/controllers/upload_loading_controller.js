6773354b5675ccc7c752d1dfb48a9e944e3875b8
Louis Alexandre Richoux <louisalex.richoux@gmail.com>
2025-12-09 09:49:26 +0100

    done

--- app/javascript/controllers/upload_loading_controller.js
+++ app/javascript/controllers/upload_loading_controller.js
@@ -7,36 +7,16 @@ export default class extends Controller {
     loading.className = 'upload-loading'
     loading.innerHTML = `
       <div class="upload-loading-content">
-        <div class="wrapper">
-          <div class="candles">
-            <div class="light__wave"></div>
-            <div class="candle1">
-              <div class="candle1__body">
-                <div class="candle1__eyes">
-                  <span class="candle1__eyes-one"></span>
-                  <span class="candle1__eyes-two"></span>
-                </div>
-                <div class="candle1__mouth"></div>
-              </div>
-              <div class="candle1__stick"></div>
-            </div>
-
-            <div class="candle2">
-              <div class="candle2__body">
-                <div class="candle2__eyes">
-                  <div class="candle2__eyes-one"></div>
-                  <div class="candle2__eyes-two"></div>
-                </div>
-              </div>
-              <div class="candle2__stick"></div>
-            </div>
-            <div class="candle2__fire"></div>
-            <div class="sparkles-one"></div>
-            <div class="sparkles-two"></div>
-            <div class="candle__smoke-one"></div>
-            <div class="candle__smoke-two"></div>
+        <div class="spinner">
+          <div class="stickman">
+            <div class="head"></div>
+            <div class="body"></div>
+            <div class="arm-left"></div>
+            <div class="arm-right"></div>
+            <div class="leg-left"></div>
+            <div class="leg-right"></div>
           </div>
-          <div class="floor"></div>
+          <div class="ground"></div>
         </div>
         <h2 style="margin-top: 3rem; color: #2c3e50; font-size: 1.8rem; font-weight: 700;">
           <i class="fas fa-brain" style="color: #1351AA; margin-right: 0.5rem;"></i>
